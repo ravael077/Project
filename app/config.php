e1ee33e0310986762ebfbb7e8985d327e388befc
ravael077 <ravael077@gmail.com>
2018-08-03 11:33:45 +0700

    Update config.php

--- app/config.php
+++ app/config.php
@@ -14,9 +14,9 @@ $conn = mysql_connect("localhost","root","root")
  */
 
 $databaseHost = 'db';
-$databaseName = 'tcc';
 $databaseUsername = 'root';
 $databasePassword = 'root';
+$databaseName = 'tcc';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  
